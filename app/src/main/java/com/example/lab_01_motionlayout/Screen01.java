87672b1ee627a3b4ca2f1b1fbae51839a11dc189
ngoctri3110 <62044877+ngoctri3110@users.noreply.github.com>
2021-09-19 11:30:12 +0700

    update screen1, screen2

--- app/src/main/java/com/example/lab_01_motionlayout/Screen01.java
+++ app/src/main/java/com/example/lab_01_motionlayout/Screen01.java
@@ -3,12 +3,30 @@ package com.example.lab_01_motionlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Screen01 extends AppCompatActivity {
 
+    private ImageView img_taylor;
+    private ImageView img_music;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen01);
+
+        img_taylor = findViewById(R.id.img_taylor);
+        img_music = findViewById(R.id.img_music);
+        Animation animation =
+                AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.anirotate);
+
+        Animation animation1 =
+                AnimationUtils.loadAnimation(getApplicationContext(),
+                        R.anim.aniblink);
+
+        img_taylor.startAnimation(animation);
+        img_music.startAnimation(animation1);
     }
 }
--- app/src/main/java/com/example/lab_01_motionlayout/Screen02.java
+++ app/src/main/java/com/example/lab_01_motionlayout/Screen02.java
@@ -1,14 +1,41 @@
 package com.example.lab_01_motionlayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-public class Screen02 extends AppCompatActivity {
+public class Screen02 extends Fragment {
 
+    private ImageView img_taylor;
+    private ImageView img_music;
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_screen02);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_screen02, container, false);
+        img_taylor = view.findViewById(R.id.img_taylor);
+        img_music = view.findViewById(R.id.img_music2);
+        Animation animation =
+                AnimationUtils.loadAnimation(view.getContext(),
+                        R.anim.anirotate);
+
+        Animation animation1 =
+                AnimationUtils.loadAnimation(view.getContext(),
+                        R.anim.aniblink);
+
+        img_taylor.startAnimation(animation);
+        img_music.startAnimation(animation1);
+        return view;
     }
+
+
+
 }
